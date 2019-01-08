45c3de97b70e48bd2e891904fff597e441d18f2c
MoulinDigital <MoulinDigital@UC20377.ladrome.local>
2019-01-08 09:22:26 +0100

    logger

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,13 +14,9 @@ public class GildedRose {
         for (Item item : items) {
 
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                logger.debug("if not Sulfuras: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
+                NoLegendaryProduct(item);
 
-                try {
-                    NoLegendaryProduct(item);
-                }
-                catch (Exception e){
-                    logger.debug("error on: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
-                }
 
             }
         }
@@ -34,9 +30,11 @@ public class GildedRose {
               || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             item.quality = QualityUp(item);
             item.quality = IfConcertOver(item);
+            logger.debug("if Brie or Stage: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
         }
         else{
             item.quality = QualityDown(item);
+            logger.debug("if not Brie or Stage: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
         }
     }
 
@@ -47,21 +45,25 @@ public class GildedRose {
     public int QualityUp(Item item){
         if (item.quality < 50) {
             item.quality ++;
+            logger.debug("quality++ : item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
 
             // si c'est une place de concert
             if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 
                 // si le nombre d'item est inf à 11, +1 +1
                 if (item.sellIn < 11 && item.quality < 50) {
-                        item.quality ++;
+                    item.quality ++;
+                    logger.debug("quality++: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
                 }
                 // si le nombre d'item est inf à 6, +1 +1 +1
                 if (item.sellIn < 6 && item.quality < 50) {
-                        item.quality ++;
+                    item.quality ++;
+                    logger.debug("quality++: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
                 }
             }
             else if (item.sellIn < 0){
                 item.quality ++;
+                logger.debug("quality++: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
             }
         }
         return item.quality;
@@ -71,11 +73,14 @@ public class GildedRose {
         int variation_quality = 0;
         if (item.quality > 0) {
             variation_quality--;
+            logger.debug("quality--: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
             if (item.name.contains("Conjured")){
                 variation_quality *= 2;
+                logger.debug("quality--*2: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
             }
             if(item.sellIn < 0){
                 variation_quality *= 2;
+                logger.debug("quality--*2: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
             }
         }
         if(item.quality + variation_quality < 0){
@@ -85,13 +90,15 @@ public class GildedRose {
     }
 
     public int SellInDown(Item item){
-            return item.sellIn --;
+        logger.debug("sillIn--: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
+        return item.sellIn --;
     }
 
     public int IfConcertOver(Item item){
         if ((item.name.equals("Backstage passes to a TAFKAL80ETC concert")) && (item.sellIn < 0)){
             return 0;
         }
+        logger.debug("not a Stage: item " + item.name + " - sellIn " + item.sellIn + " - quality " + item.quality);
         return item.quality;
     }
 }
